6ba2dbc2641dfec9c6ce1af34d94e7b7466f1f73
Melanie Desaive <desaive@gmx.de>
2016-01-23 00:59:20 +0100

    Cleaned up settings.py

---
+++ admin/settings_secret.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        # 'ENGINE': 'mysql.connector.django',
+        'NAME': 'admin',
+        'USER': 'django',
+        'PASSWORD': 'Etae%w8A',
+        'HOST': 'localhost',
+    }
+}
+
+
